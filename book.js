1d3aeceadb6c70231c45ad7e162a3351a7495054
Atul Vishwakarma <atulvishw240@gmail.com>
2026-03-11 14:05:37 +0530

    Throw error when constructor is called without 'new'

--- book.js
+++ book.js
@@ -1,6 +1,10 @@
 const myLibrary = [];
 
 function Book(id, title, author, noOfPages, isRead) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+
   this.id = id;
   this.title = title;
   this.author = author;
